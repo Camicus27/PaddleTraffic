1a089a967ea6e4b8caa566a6ced80ef7f3438433
Jacob Knowlton <u1117344@utah.edu>
2023-11-20 12:40:51 -0700

    Added header component

---
+++ frontend/src/components/CommonFooter.vue
@@ -0,0 +1,7 @@
+<script setup  lang="ts">
+
+</script>
+
+<template>
+    <h1>This is the footer component!</h1>
+</template>
---
+++ frontend/src/components/CommonHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="header">
+    <img src="@/assets/logo.png" class="logo" alt="PaddleTraffic Logo" width="125" height="125" />
+    <h1>PaddleTraffic</h1>
+    <a href="">Map</a>
+    <a href="">Matchmaking</a>
+    <a href="">About</a>
+  </div>
+</template>
+
+<style scoped>
+.header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background-color: #9fb184;
+}
+
+.header h1 {
+    padding: .5em;
+}
+
+.header a {
+    padding: 1em;
+}
+</style>
