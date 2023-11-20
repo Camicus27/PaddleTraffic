7b916b20135fe23847862a533ae04b30e55fd14c
Jacob Knowlton <u1117344@utah.edu>
2023-11-20 14:47:17 -0700

    Renamed sidebar to follow Vue guidelines

---
+++ frontend/src/components/CommonSidebar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import SidebarItem from './SidebarItem.vue'
+
+let matchList = ref([
+  { id: 1, loc: 'The playground', time: '12:00 pm', skillLvl: 'beginner' },
+  { id: 2, loc: 'Picklecoin Tournament', time: '3:00 pm', skillLvl: 'advanced' }
+])
+</script>
+
+<template>
+  <div class="sidebar">
+    <SidebarItem
+      v-for="{ id, loc, time, skillLvl } in matchList"
+      :key="id"
+      :location="loc"
+      :time="time"
+      :skillLevel="skillLvl"
+    />
+  </div>
+</template>
+
+<style scoped></style>
