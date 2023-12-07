56f474cdb47852be111b06b9fe84a8968f8a35ab
Jacob Knowlton <u1117344@utah.edu>
2023-12-06 22:49:43 -0700

    Added content to the footer

--- frontend/src/components/CommonFooter.vue
+++ frontend/src/components/CommonFooter.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>This is the footer component!</h1>
+  <footer>
+    <div>
+      <h3>PaddleTraffic</h3>
+      <p>Connecting Pickleball Players to Courts Everywhere</p>
+    </div>
+    <div>
+      <p>Contact us at: <a href="mailto:support@paddletraffic.com">support@paddletraffic.com</a></p>
+      <p>Follow us on social media:</p>
+      <a href="https://www.facebook.com/PaddleTraffic" target="_blank">Facebook</a> |
+      <a href="https://twitter.com/PaddleTraffic" target="_blank">Twitter</a> |
+      <a href="https://www.instagram.com/PaddleTraffic" target="_blank">Instagram</a>
+    </div>
+    <div>
+      <p>&copy; 2023 PaddleTraffic. All rights reserved.</p>
+    </div>
+  </footer>
 </template>
+
+<style scoped>
+footer {
+  background-color: #9fb184;
+  padding: 20px;
+  text-align: center;
+}
+</style>
