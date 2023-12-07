4946749bb6423e6afb77f0f97ebae22e5eb8c218
Jacob Knowlton <u1117344@utah.edu>
2023-12-06 22:08:07 -0700

    Fixed the sidebar to have all the correct fields from the database.

--- frontend/src/components/CommonSidebar.vue
+++ frontend/src/components/CommonSidebar.vue
@@ -1,22 +1,47 @@
-<script setup lang="ts">
+<script setup lang='ts'>
 import { ref } from 'vue'
+import axios from 'axios'
 import SidebarItem from './SidebarItem.vue'
 
+// name = models.CharField(max_length=255)
+// location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='scheduled_events', null=False)
+// host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hosted_events', null=False)
+// players = models.ManyToManyField(User, related_name='attending_events', blank=True)
+// date = models.DateField()
+// time = models.TimeField()
+
 let matchList = ref([
-  { id: 1, loc: 'The playground', time: '12:00 pm', skillLvl: 'beginner' },
-  { id: 2, loc: 'Picklecoin Tournament', time: '3:00 pm', skillLvl: 'advanced' },
-  { id: 3, loc: 'Picklecoin HQ', time: '4:30 pm', skillLvl: 'intermediate' }
+  { id: 1, name: 'Kids Tournament', location: 'The playground', host: 'Marissa Langley', players: ['Elena Rodriguez', 'Kaito Nakamura', 'Amina Yusuf'], date: '12/10/2023', time: '12:00 pm' },
+  { id: 2, name: 'Thursday Night Pickleball', location: 'SLC Court', host: 'Rajiv Patel', players: ['Lucas Schmidt', 'Jordan Abrams'], date: '1/17/2024', time: '3:00 pm' },
+  { id: 3, name: 'Picklecoin Tournament', location: 'Picklecoin HQ', host: 'Theo Zimmerman', players: [], date: '3/28/2024', time: '4:30 pm' }
 ])
+
+// Uncomment the below code after the prototype demo. The above is dummy data for the events a user is part of
+
+// let URL: string
+// // This is the collection of environment variables.
+// const env = import.meta.env
+// if (env.MODE === 'production')
+//   URL = env.VITE_PROD_URL
+// else
+//   URL = env.VITE_DEV_URL
+
+// axios.get(`${URL}/events`)
+//       .then((response) => matchList.value = response.data)
+//       .catch((error) => console.log(error))
 </script>
 
 <template>
-  <div class="sidebar">
+  <div class='sidebar'>
     <SidebarItem
-      v-for="{ id, loc, time, skillLvl } in matchList"
+      v-for="{ id, name, location, host, players, date, time } in matchList"
       :key="id"
-      :location="loc"
+      :name="name"
+      :location="location"
+      :host="host"
+      :players="players"
+      :date="date"
       :time="time"
-      :skillLevel="skillLvl"
     />
   </div>
 </template>
--- frontend/src/components/SidebarItem.vue
+++ frontend/src/components/SidebarItem.vue
@@ -1,22 +1,34 @@
 <script setup lang="ts">
 defineProps<{
+  name?: string
   location?: string
+  host?: string
+  players?: Array<string>
+  date?: string
   time?: string
-  skillLevel?: string
 }>()
 </script>
 
 <template>
   <div class="sidebar-item">
-    <h3>Match at {{ location }}</h3>
-    <h3>Time: {{ time }}</h3>
-    <h3>Skill level: {{ skillLevel }}</h3>
+    <h2>{{ name }}</h2>
+    <h3>Players:</h3>
+    <ul>
+      <!-- Maybe shouldn't use player for the key, since not guaranteed to be unique -->
+      <li>{{ host }} (Host)</li>
+      <li v-for="player in players" :key="player">{{ player }}</li>
+    </ul>
+    <h3>Match at {{ location }}, {{ date }} {{ time }}</h3>
   </div>
 </template>
 
 <style scoped>
-    .sidebar-item {
-        border: 3px solid black;
-        background-color: lightsteelblue;
-    }
+.sidebar-item {
+  text-align: center;
+  width: 15em;
+  border: 3px solid black;
+  background-color: lightsteelblue;
+  padding: .5em;
+  margin: .5em;
+}
 </style>
