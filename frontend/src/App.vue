9927109c21edb8e8aad7daf34016419e99cbec80
Jacob Knowlton <u1117344@utah.edu>
2023-12-06 21:05:02 -0700

    Added a CSS reset stylesheet

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,6 +23,38 @@ import CommonFooter from './components/CommonFooter.vue'
 <style scoped>
   .main-wrapper {
     display: flex;
-    
+  }
+</style>
+
+<!-- A css reset stylesheet is included in the index.html file, so global styles can be placed here -->
+<style>
+  h1 {
+    font-size: 2em;
+    font-weight: bolder;
+  }
+
+  h2 {
+    font-size: 1.5em;
+    font-weight: bolder;
+  }
+
+  h3 {
+    font-size: 1.17em;
+    font-weight: bolder;
+  }
+
+  h4 {
+    font-size: 1em;
+    font-weight: bolder;
+  }
+  
+  h5 {
+    font-size: .83em;
+    font-weight: bolder;
+  }
+
+  h6 {
+    font-size: .67em;
+    font-weight: bolder;
   }
 </style>
