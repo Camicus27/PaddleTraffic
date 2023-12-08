3020017d8214d9d5df0b52486e27b9a1d2c6447c
Ian McBride <u1275006@utah.edu>
2023-12-07 17:31:00 -0700

    Added GeoLocation functionality

--- frontend/src/components/PickleballMap.vue
+++ frontend/src/components/PickleballMap.vue
@@ -2,18 +2,36 @@
 import { ref, onMounted, onUnmounted, type Ref } from 'vue'
 import mapboxgl from "mapbox-gl";
 
-const mapContainer = ref()
+const mapContainer = ref();
 
+// Will improve token handling
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3VudHp1Y2Fwc3RvbmUiLCJhIjoiY2xwOHl6MGZiMWQwcjJ2bzNpdTh3ZXZ5diJ9.2OxP9v87qKxpFmL7FFrD-g';
-const map: Ref<mapboxgl.Map | undefined> = ref()
+const map: Ref<mapboxgl.Map | undefined> = ref();
+
 onMounted(() => {
   map.value = new mapboxgl.Map({
     container: mapContainer.value,
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-111.876183, 40.758701],
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-111.876183, 40.758701], // Default to SLC
     zoom: 11
   });
 
+  const geolocateControl = new mapboxgl.GeolocateControl({
+    fitBoundsOptions: { maxZoom: 11 },
+    positionOptions: { enableHighAccuracy: true },
+    trackUserLocation: true,
+    // showUserHeading: true
+  });
+  
+  // Add location button to map
+  map.value.addControl(geolocateControl);
+
+  // Get location on load
+  map.value.on('load', () => {
+    geolocateControl?.trigger();
+  });
+
+  // Todo: Get locations from database and dynamically add markers
   const popup = new mapboxgl.Popup().setText('This is a pickleball location!');
 
   new mapboxgl.Marker()
