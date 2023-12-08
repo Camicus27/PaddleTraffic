ee54cf143ed44c8f41cb95f4f18b1aaa73aa509c
Ian McBride <u1275006@utah.edu>
2023-12-08 00:20:11 -0700

    Added dynamic location fetching and updating. Added report form.

--- frontend/src/components/PickleballMap.vue
+++ frontend/src/components/PickleballMap.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted, type Ref } from 'vue'
 import mapboxgl from "mapbox-gl";
+import axios from 'axios'
 
 interface Location {
+  id: number;
   name: string;
   latitude: number;
   longitude: number;
@@ -13,11 +15,20 @@ interface Location {
 }
 
 const mapContainer = ref();
-const infoSection = ref("Select a location for more info");
+const currSelection = ref<Location | undefined>();
+const allLocations: Ref<Location[]> = ref([])
 
 // Todo: improve token handling
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3VudHp1Y2Fwc3RvbmUiLCJhIjoiY2xwOHl6MGZiMWQwcjJ2bzNpdTh3ZXZ5diJ9.2OxP9v87qKxpFmL7FFrD-g';
 const map: Ref<mapboxgl.Map | undefined> = ref();
+
+let URL: string
+// This is the collection of environment variables.
+const env = import.meta.env
+if (env.MODE === 'production')
+  URL = env.VITE_PROD_URL
+else
+  URL = env.VITE_DEV_URL
 
 // Get users geolocation if permitted
 function initGeoloc(mapVal: mapboxgl.Map) {
@@ -33,39 +44,48 @@ function initGeoloc(mapVal: mapboxgl.Map) {
 
 // Add markers to map
 function addMarkers(mapVal: mapboxgl.Map) {
-  // Todo: get locations from backend
-  const locations: Location[] = [
-    {
-      name: "Hogan Park",
-      latitude: 40.874056,
-      longitude: -111.901012,
-      court_count: 6,
-      courts_occupied: 3,
-      number_waiting: 0,
-      estimated_wait_time: 0
-    }
-  ];
-
-  locations.forEach(loc => {
-    const marker = new mapboxgl.Marker()
-      .setLngLat([loc.longitude, loc.latitude])
-      .addTo(mapVal);
-    // Add an event listener to each marker
-    marker.getElement().addEventListener('click', () => {
-      updateInfoSection(loc);
-    });
-  });
+  axios.get(`${URL}/locations/`)
+    .then((response) => {
+      allLocations.value = response.data.locations
+      allLocations.value.forEach(loc => {
+        const marker = new mapboxgl.Marker()
+          .setLngLat([loc.longitude, loc.latitude])
+          .addTo(mapVal);
+        // Add an event listener to each marker
+        marker.getElement().addEventListener('click', () => {
+          updateInfoSection(loc.id);
+        });
+      });
+    })
+    .catch((error) => console.log(error))
 }
 
 // Update the info section with location data
-function updateInfoSection(loc: Location) {
-  infoSection.value = `
-    ${loc.name} <br>
-    Number of Courts: ${loc.court_count} <br>
-    Courts Occupied: ${loc.courts_occupied} <br>
-    Number Waiting: ${loc.number_waiting} <br>
-    Estimated Wait Time: ${loc.estimated_wait_time} minutes
-  `;
+function updateInfoSection(locId: number) {
+  currSelection.value = allLocations.value.find(location => location.id === locId);
+}
+
+const locForm = ref({
+  courts_occupied: 0,
+  number_waiting: 0
+})
+
+function submitForm() {
+  axios.post(`${URL}/locations/${currSelection.value?.id}/report/`, { report: locForm.value })
+    .then(response => {
+      // Handle the response here. For example, logging the new location ID.
+      console.log('New event ID:', response.data);
+      axios.get(`${URL}/locations/`)
+        .then((response) => {
+          allLocations.value = response.data.locations
+        })
+        .catch((error) => console.log(error))
+        currSelection.value = response.data.location
+    })
+    .catch(error => {
+      // Handle errors here
+      console.error('Error:', error);
+    });
 }
 
 onMounted(() => {
@@ -91,7 +111,25 @@ onUnmounted(() => {
 <template>
   <div class="container">
     <div ref="mapContainer" class="map-container"></div>
-    <div class="info-section" v-html="infoSection"></div>
+    <div class="info-section flex-row" v-if="currSelection">
+      <div class="info padding-x">
+        <h3>{{ currSelection.name }}</h3>
+        <p>Number of Courts: {{ currSelection.court_count }}</p>
+        <p>Courts Occupied: {{ currSelection.courts_occupied }}</p>
+        <p>Number Waiting: {{ currSelection.number_waiting }}</p>
+        <p>Estimated Wait Time: {{ currSelection.estimated_wait_time }}</p>
+      </div>
+      <div class="padding-x">
+        <form @submit.prevent="submitForm">
+          <label for="courtsOccupied">Courts Occupied:</label><br>
+          <input type="number" id="courtsOccupied" name="courtsOccupied" min="0" v-model="locForm.courts_occupied"><br><br>
+          <label for="numberWaiting">Number Waiting:</label><br>
+          <input type="number" id="numberWaiting" name="numberWaiting" min="0" v-model="locForm.number_waiting"><br><br>
+          <input type="submit" value="Update Status">
+        </form>
+      </div>
+    </div>
+    <div class="info-section" v-else>Select a court for more information</div>
   </div>
 </template>
 
@@ -115,8 +153,60 @@ onUnmounted(() => {
   background-color: lightsteelblue;
 }
 
-.info-section::first-line {
+.flex-row {
+  display: flex;
+  justify-content: space-between;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  font-size: larger;
+  justify-content: center;
+}
+
+.info > p {
+  margin-top: 0.25em;
+}
+
+.padding-x {
+  padding-left: 1em;
+  padding-right: 1em;
+}
+
+form {
+  background-color: #D0D4CA;
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+label {
+  color: #555;
+  font-size: 14px;
   font-weight: bold;
-  font-size: large;
+  display: block;
+}
+
+input[type="number"] {
+  width: 100%;
+  padding: 3px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type="submit"] {
+  width: 100%;
+  background-color: #9ab375;
+  color: white;
+  padding: 3px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type="submit"]:hover {
+  background-color: #69794f;
 }
 </style>
