d5d3b112e9ae21401fdbd8fc61e14eafe84556cb
Ian McBride <u1275006@utah.edu>
2023-12-07 19:28:13 -0700

    Added interactible markers and info section that populates based on selected location. Improved the CSS.

--- frontend/src/components/PickleballMap.vue
+++ frontend/src/components/PickleballMap.vue
@@ -2,7 +2,18 @@
 import { ref, onMounted, onUnmounted, type Ref } from 'vue'
 import mapboxgl from "mapbox-gl";
 
+interface Location {
+  name: string;
+  latitude: number;
+  longitude: number;
+  court_count: number;
+  courts_occupied: number;
+  number_waiting: number;
+  estimated_wait_time: number;
+}
+
 const mapContainer = ref();
+const infoSection = ref("Select a location for more info");
 
 // Todo: improve token handling
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3VudHp1Y2Fwc3RvbmUiLCJhIjoiY2xwOHl6MGZiMWQwcjJ2bzNpdTh3ZXZ5diJ9.2OxP9v87qKxpFmL7FFrD-g';
@@ -22,24 +33,39 @@ function initGeoloc(mapVal: mapboxgl.Map) {
 
 // Add markers to map
 function addMarkers(mapVal: mapboxgl.Map) {
-  // Todo: get locations from database
-  const locations = [
+  // Todo: get locations from backend
+  const locations: Location[] = [
     {
       name: "Hogan Park",
       latitude: 40.874056,
       longitude: -111.901012,
       court_count: 6,
       courts_occupied: 3,
-      number_waiting: 10,
-      estimated_wait_time: "15m"
+      number_waiting: 0,
+      estimated_wait_time: 0
     }
   ];
 
   locations.forEach(loc => {
-    new mapboxgl.Marker()
+    const marker = new mapboxgl.Marker()
       .setLngLat([loc.longitude, loc.latitude])
       .addTo(mapVal);
+    // Add an event listener to each marker
+    marker.getElement().addEventListener('click', () => {
+      updateInfoSection(loc);
+    });
   });
+}
+
+// Update the info section with location data
+function updateInfoSection(loc: Location) {
+  infoSection.value = `
+    ${loc.name} <br>
+    Number of Courts: ${loc.court_count} <br>
+    Courts Occupied: ${loc.courts_occupied} <br>
+    Number Waiting: ${loc.number_waiting} <br>
+    Estimated Wait Time: ${loc.estimated_wait_time} minutes
+  `;
 }
 
 onMounted(() => {
@@ -63,13 +89,34 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div ref="mapContainer" class="map-container"></div>
+  <div class="container">
+    <div ref="mapContainer" class="map-container"></div>
+    <div class="info-section" v-html="infoSection"></div>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  padding: 1em;
+  width: 100%;
+}
+
 .map-container {
-  flex: 1;
-  margin: 2em;
-  height: 500px;
+  min-height: 40em;
+}
+
+.info-section {
+  margin-top: 1em;
+  padding: 1em;
+  border: 3px solid black;
+  background-color: lightsteelblue;
+}
+
+.info-section::first-line {
+  font-weight: bold;
+  font-size: large;
 }
 </style>
