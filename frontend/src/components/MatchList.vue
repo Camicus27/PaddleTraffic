b73b825c54b8d821d9c8a4d050fcfa18e30a0b2e
Camron Wilson <camronkwilson@gmail.com>
2024-02-09 13:32:27 -0700

    Changed match event serialization

--- frontend/src/components/MatchList.vue
+++ frontend/src/components/MatchList.vue
@@ -50,13 +50,21 @@ function getAllPlayers() {
   <template v-if="!isFetching">
     <div class="matches" v-for="{ id, name, location, host, players, date, time } in matches" :key="id">
       <h2>{{ name }}</h2>
-      <h3>Players:</h3>
-      <ul>
-        <!-- Maybe shouldn't use player for the key, since not guaranteed to be unique -->
-        <li>{{ allPlayers.filter((p: any) => p.id === host)[0].username }} (Host)</li>
-        <li v-for="player in players" :key="player">{{ allPlayers.filter((p: any) => p.id === player)[0].username }}</li>
-      </ul>
-      <h3>Match at {{ location }}, {{ date }} {{ time }}</h3>
+      <h3>Host:</h3>
+      <p>{{ allPlayers.filter((p: any) => p.id === host)[0].username }}</p>
+      <div class="players" v-if="players.length > 0"> <!-- v-if="0" -->
+        <h3>Players:</h3>
+        <ul>
+          <!-- Maybe shouldn't use player for the key, since not guaranteed to be unique -->
+          <li v-for="player in players" :key="player">{{ allPlayers.filter((p: any) => p.id === player)[0].username }}</li>
+        </ul>
+      </div>
+      
+      <h3>Match being held at {{ location }}, {{ date }} {{ time.split('') }}</h3>
     </div>
+
+
+
+    
   </template>
 </template>
