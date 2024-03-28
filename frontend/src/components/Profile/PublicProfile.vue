6d72b0b2ee9921b8a1baa44dac0519c457a85076
Jacob Knowlton <u1117344@utah.edu>
2024-03-28 15:59:49 -0600

    Added components for public and private profile pages

---
+++ frontend/src/components/Profile/PublicProfile.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref, onMounted, watch, type Ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+import axios from 'axios'
+
+import { getCurrentUser, getFriendRequests } from '../../api/functions';
+import type { FriendRequest, PickleUser, RestrictedUser } from '../../api/types';
+</script>
+
+<template>
+    <h1>Public Profile</h1>
+</template>
+
+<style scoped lang="scss"></style>
