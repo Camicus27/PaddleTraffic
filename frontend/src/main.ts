55e0c97a4c5b4e29c09fa10729aacd9c7c75b8d6
Jacob Knowlton <u1117344@utah.edu>
2024-03-28 23:33:15 -0600

    Fixed issues with vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +15,16 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(createVuetify())
+app.use(createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi
+        }
+    }
+}))
 
 app.mount('#app')
