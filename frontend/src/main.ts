eb44f0226b0bc072814bda984021d39419bfcc6e
Jacob Knowlton <u1117344@utah.edu>
2024-03-29 14:08:31 -0600

    Moved vuetify to separate plugin

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,30 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(createVuetify({
-    components,
-    directives,
-    icons: {
-        defaultSet: 'mdi',
-        aliases,
-        sets: {
-            mdi
-        }
-    }
-}))
+app.use(vuetify)
 
 app.mount('#app')
