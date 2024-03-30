49b6fe5a52f6a3202b0bce59658c97652747b92f
brensen11 <90063365+brensen11@users.noreply.github.com>
2024-03-29 20:05:46 -0600

    features

--- b.ps1
+++ b.ps1
@@ -1,7 +1,25 @@
+param(
+    [switch]$Migrate,
+    [switch]$Clean
+)
+
 Set-Location "./backend/django_project/"
-try {
-    poetry run python manage.py runserver
+
+function MakeAndMigrate { # do we also need makemigrations paddle_traffic and migrate paddle_traffic?
+    poetry run python manage.py makemigrations
+    poetry run python manage.py migrate
 }
-finally {
-    Set-Location "../../"
+
+if($Migrate) {
+    MakeAndMigrate
+} elseif ($Clean) {
+    Remove-Item db.sqlite3 -Force
+    MakeAndMigrate
+} else {
+    try {
+        poetry run python manage.py runserver
+    }
+    finally {
+        Set-Location "../../"
+    }
 }
