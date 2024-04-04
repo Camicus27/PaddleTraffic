6b23e1b0809d829882f223ab5d576419abf3daad
brensen11 <90063365+brensen11@users.noreply.github.com>
2024-04-04 02:16:41 -0600

    Script changes, Clean works on backend script, and frontend add -- --host flag for public hosting

--- b.ps1
+++ b.ps1
@@ -7,6 +7,7 @@ Set-Location "./backend/django_project/"
 
 function MakeAndMigrate { # do we also need makemigrations paddle_traffic and migrate paddle_traffic?
     poetry run python manage.py makemigrations
+    poetry run python manage.py makemigrations paddle_traffic
     poetry run python manage.py migrate
 }
 
@@ -14,7 +15,11 @@ if($Migrate) {
     MakeAndMigrate
 } elseif ($Clean) {
     Remove-Item db.sqlite3 -Force
+    Remove-Item -Path "paddle_traffic/migrations" -Recurse -Force
+    echo "Deleted db.sqlite3"
     MakeAndMigrate
+    echo "MakeAndMigrate Finished"
+    poetry run python manage.py seed
 } else {
     try {
         poetry run python manage.py runserver
